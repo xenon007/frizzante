7277b3cd05f2e6c7c6508c75aae80c06d1f7866e
Razvan <razvan@razshare.dev>
2025-01-11 08:12:50 +0100

    feat(libsvelte_test): testing without ssr.

--- libsvelte_test.go
+++ libsvelte_test.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-func TestEchoSvelte(test *testing.T) {
+func TestEchoSvelteWithSsr(test *testing.T) {
 	server := ServerCreate()
 	ServerWithPort(server, 8083)
 	ServerWithHostName(server, "127.0.0.1")
@@ -24,6 +24,35 @@ func TestEchoSvelte(test *testing.T) {
 
 	expected := "<h1>Hello world.</h1>"
 	actual, getError := HttpGet("http://127.0.0.1:8083/")
+	if getError != nil {
+		test.Fatal(getError)
+	}
+
+	ok := strings.Contains(actual, expected)
+
+	if !ok {
+		test.Fatalf("server was expected to respond with a string that contains '%s', received '%s' instead", expected, actual)
+	}
+}
+
+func TestEchoSvelteWithoutSsr(test *testing.T) {
+	server := ServerCreate()
+	ServerWithPort(server, 8084)
+	ServerWithHostName(server, "127.0.0.1")
+	ServerWithEmbeddedFileSystem(server, embeddedFileSystem)
+	ServerOnError(server, func(err error) {
+		test.Fatal(err)
+	})
+	ServerOnRequest(server, "GET /", func(server *Server, request *Request, response *Response) {
+		EchoSvelte(response, false, map[string]interface{}{
+			"name": "world",
+		})
+	})
+	go ServerStart(server)
+	time.Sleep(1 * time.Second)
+
+	expected := "<div id=\"app\"></div>"
+	actual, getError := HttpGet("http://127.0.0.1:8084/")
 	if getError != nil {
 		test.Fatal(getError)
 	}
