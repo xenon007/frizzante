c25ab51238bbb928f3e5db165129168127d9fa5e
Razvan <razvan@razshare.dev>
2025-01-25 20:32:51 +0100

    feat(libsvelte): fixing an issue where the client application would not clear the server side application render before mounting.

--- vite-project/render.client.js
+++ vite-project/render.client.js
@@ -1,4 +1,5 @@
 import { mount } from "svelte";
 import RenderClient from "./render.client.svelte";
 // @ts-ignore
+target().innerHTML = "";
 mount(RenderClient, { target: target(), props: props() });
