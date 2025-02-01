6f84338300fc8d5e7844b9f668593f892e773393
Razvan <razvan@razshare.dev>
2025-02-01 06:09:41 +0100

    feat(libsvelte): adding context function "path", which converts a page id into a full path.

--- vite-project/render.client.svelte
+++ vite-project/render.client.svelte
@@ -6,29 +6,37 @@
     let reactiveData = $state({...data})
     setContext("data", reactiveData)
     setContext("page", page)
+    setContext("path", path)
     window.addEventListener('popstate', (event) => {
         page(event.state.pageId)
     });
     function escapeRegExp(string) {
         return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
     }
-    function page(pageId){
+    function path(pageId){
+        let path = paths[pageId]??''
         if (!paths[pageId]) {
-            return
+            return ""
         }
-        let path = paths[pageId]
         const resolved = {}
-        for(let key in data) {
+        for(let key in data.path) {
             resolved[key] = false
         }
-        for(let key in data) {
+        for(let key in data.path) {
             const value = data[key]
             const regex = escapeRegExp(`{${key}}`)
             let oldPath = path
             path = path.replaceAll(new RegExp(regex,'g'), value)
             resolved[key] = oldPath === path
         }
-        history.pushState({pageId}, '', path);
+
+        return path
+    }
+    function page(pageId){
+        if (!paths[pageId]) {
+            return
+        }
+        history.pushState({pageId}, '', path(pageId));
         reactivePageId = pageId
     }
 </script>
