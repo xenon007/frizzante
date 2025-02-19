f9580a3f8206ce4ffe608a98d472e65082a9d7f6
Razvan <razvan@razshare.dev>
2025-02-19 19:41:46 +0100

    feat(libsvelte): fixing an issue that would case the frontend app to render twice.

--- vite-project/render.client.js
+++ vite-project/render.client.js
@@ -1,5 +1,5 @@
 import { mount } from "svelte";
 import RenderClient from "./render.client.svelte";
 // @ts-ignore
-
+target().innerHTML = "";
 mount(RenderClient, { target: target(), props: props() });
