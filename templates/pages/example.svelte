33669d1f045f893c374815c852c3236c67cdf40c
Razvan <razvan@razshare.dev>
2025-04-06 23:30:06 +0200

    feat(libmake): page template example now defines a Data type using JSDoc.

--- templates/pages/example.svelte
+++ templates/pages/example.svelte
@@ -1,5 +1,13 @@
 <script>
     import {getContext} from "svelte";
+
+    /**
+     * Server data.
+     * @typedef Data
+     * @property {}
+     */
+
+    /** @type {Data} */
     const data = getContext("data")
 </script>
 
